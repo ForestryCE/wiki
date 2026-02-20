81c27dbc26f27ab4d490080a6b0e03af73aec95c
bluffcon <nartiss.cat@gmail.com>
2026-02-20 19:05:27 +0300

    add home page thumbs up

---
+++ docs/pageindex.js
@@ -0,0 +1,4 @@
+const data = [
+  { title: "Forest Bee", link: "https://forestryce.github.io/wiki/Forest+Bee/", desc: "Common honey-bearing forest pest"},
+  { title: "Main Page", link: "https://forestryce.github.io/wiki/", desc: "The Home of Forestry:CE Wiki"}
+];
