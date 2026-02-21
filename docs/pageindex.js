cf9ce7920c2b796e30c2de3f427ca3b4d9823a2c
bluffcon <nartiss.cat@gmail.com>
2026-02-21 13:11:52 +0300

    update mobile view again and again and again and again and again and again and again and

--- docs/pageindex.js
+++ docs/pageindex.js
@@ -1,4 +1,5 @@
 const data = [
     { title: "Main Page", link: "https://forestryce.github.io/wiki/", desc: "The very Home of Forestry:CE Wiki"},
     { title: "Forest Bee", link: "https://forestryce.github.io/wiki/Forest+Bee/", desc: "Common honey-bearing forest pest"}
+    
 ]
